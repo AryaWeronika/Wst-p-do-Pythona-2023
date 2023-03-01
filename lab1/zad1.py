b0653d08ef4fe60138bbe7b0ea4e1d6819b774d9
AryaWeronika <74681098+AryaWeronika@users.noreply.github.com>
2023-03-01 21:24:46 +0100

    Add files via upload

---
+++ lab1/zad1.py
@@ -0,0 +1,9 @@
+#Przestudiuj dokumentację opisującą typ int oraz float i napisz kod, w którym stworzysz
+#  po dwa obiekty typu int i float tak z użyciem różnych wartośxi 
+# dla konstruktorów tych typów (np. z inną podstawą niż domyślne 10 dla typu int).
+stala1 = 9
+stala2 = int(0b100000)
+zm1 = 8.5
+zm2 = float(-0b010000)
+print(stala1, stala2)
+print(zm1, zm2)
---
+++ lab1/zad3.py
@@ -0,0 +1,7 @@
+#Przygotuj 4 przykłady kodu, który będzie wykorzystywał operatory bitowe.
+o1 = 0b1
+o2 = 0b1100
+print(o1, o2 << 3)
+print(o1, o2 >> 5)
+print(~o1, ~~o2)
+print(0b0 & 0b1, 0b1 & 0b1)
